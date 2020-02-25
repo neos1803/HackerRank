7f9a3f59c4c858d7bdb7fb066c1cb97f938d3074
Nanda W. Purba <38176579+neos1803@users.noreply.github.com>
2020-02-25 15:15:16 +0700

    Create array-reversal.c

---
+++ C/Medium/array-reversal.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, *temp, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    temp = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+
+    /* Write the logic to reverse the array. */
+    for(i = num-1; i >=0; i--)
+        printf("%d ", *(arr + i));
+    return 0;
+}
