b3481e6429f0ab67f8c342712e6dceea33e54101
Nanda W. Purba <38176579+neos1803@users.noreply.github.com>
2020-03-30 11:04:42 +0700

    Day 6 of 30 Days of Code

--- 30-Days-Of-Code/Python-3/even-odd-letters.py
+++ 30-Days-Of-Code/Python-3/even-odd-letters.py
@@ -1,4 +1,4 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-for x in range (int (input())) :
+for _ in range (int (input())) :
     w = input();
     print(w[::2], w[1::2])
