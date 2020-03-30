18a1020a00a00476ef6e43f04ed97a6a320c320d
Nanda W. Purba <38176579+neos1803@users.noreply.github.com>
2020-03-30 11:10:05 +0700

    Day 2 of 30 Days of Code

--- 30-Days-Of-Code/Python-3/operators.py
+++ 30-Days-Of-Code/Python-3/operators.py
@@ -6,7 +6,6 @@ import random
 import re
 import sys
 
-# Complete the solve function below.
 def solve(meal_cost, tip_percent, tax_percent):
     total_cost = meal_cost*tip_percent/100 + meal_cost*tax_percent/100 + meal_cost
     print(round(total_cost))
