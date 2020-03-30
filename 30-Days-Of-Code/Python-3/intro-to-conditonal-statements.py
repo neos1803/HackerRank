4d66ee7473e73e43fb6e83932b6b68e64ca178ae
Nanda W. Purba <38176579+neos1803@users.noreply.github.com>
2020-03-30 11:08:33 +0700

    Day 3 of 30 Days of Code

--- 30-Days-Of-Code/Python-3/intro-to-conditonal-statements.py
+++ 30-Days-Of-Code/Python-3/intro-to-conditonal-statements.py
@@ -9,13 +9,13 @@ import sys
 
 
 if __name__ == '__main__':
-    N = int(input())
-    if N % 2 == 0 :
-        if N >= 2 and N <= 5 :
+    n = int(input())
+    if n % 2 == 0 :
+        if n >= 2 and n <= 5 :
             print("Not Weird")
-        elif N >=6 and N <= 20 :
+        elif n >=6 and n <= 20 :
             print("Weird")
-        elif N > 20:
+        elif n > 20:
             print("Not Weird")
     else:
         print("Weird")
